b510be2c00078695c6600a4076141f12b60095e6
Ganesh Sandeep Kanumilli <89462870+SandeepKanumilli@users.noreply.github.com>
2025-02-05 11:36:51 -0600

    For loop recursion approach

--- CombinationSum.py
+++ CombinationSum.py
@@ -1,29 +1,66 @@
-
-# Time Complexity : O(2^(n+m) * N)
-# Space Complexity : O((n+m) * 2^(n+m))
-
+#For loop Recursion Approach
+# TC = O(2^n+m//2)
+#SC = O(n+m)
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         if candidates == None and len(candidates) == 0:
             return []
         self.result = []
-        self.recurcive(candidates, target, 0, [] )
+        self.recursion(candidates, target, 0, [])
         return self.result
     
-    def recurcive(self, candidates: List[int], target: int, index: int, path : List[int] ) -> None:
-        if(target == 0):
+    def recursion(self, canidates : List[int], target : int, piviot : int, path : List[int]) -> None:
+        if target == 0:
             self.result.append([num for num in path])
-            return 
-        if index == len(candidates) or target < 0:
-            return 
+            return   
+        if target < 0 or piviot == len(target):
+            return
         
-        #0 Case
+        #0 case
+        self.recursion(canidates, target , piviot + 1, path)
+        
+        # 1 case
+        for i in range(len(canidates)):
+            path.append(canidates[i])
+            self.recursion(canidates, target - canidates[i], i , path)
+            path.pop()
+        
+            
 
-        self.recurcive(candidates, target, index+1, [num for num in path])
 
-        #1 Case
-        path.append(candidates[index])
-        self.recurcive(candidates, target - candidates[index], index , [num for num in path])
-        path.pop()
+
+
+
+
+
+
+
+
+# Time Complexity : O(2^(n+m) * N)
+# Space Complexity : O((n+m) * 2^(n+m))
+
+# class Solution:
+#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+#         if candidates == None and len(candidates) == 0:
+#             return []
+#         self.result = []
+#         self.recurcive(candidates, target, 0, [] )
+#         return self.result
+    
+#     def recurcive(self, candidates: List[int], target: int, index: int, path : List[int] ) -> None:
+#         if(target == 0):
+#             self.result.append([num for num in path])
+#             return 
+#         if index == len(candidates) or target < 0:
+#             return 
+        
+#         #0 Case
+
+#         self.recurcive(candidates, target, index+1, [num for num in path])
+
+#         #1 Case
+#         path.append(candidates[index])
+#         self.recurcive(candidates, target - candidates[index], index , [num for num in path])
+#         path.pop()
 
         
